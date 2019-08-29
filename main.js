444ce90f5391b0a3283c0f3d9c416e65cb276548
Joseph Young <jsyoung0891@gmail.com>
2019-08-29 17:08:10 +0100

    finished typewriter add on. need to style and reformat

--- main.js
+++ main.js
@@ -52,15 +52,46 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // TYPEWRITER
 
-  const TypeWriter = function(textElement, words, delayTime = 2000) {
-    this.textElement = textElement;
-    this.words = words;
-    this.txt = "";
-    this.wordIndex = 0;
-    this.delayTime = parseInt(delayTime, 10);
-    this.type();
-    this.isDeleting = false;
-  };
+  class TypeWriter {
+    constructor(textElement, words, delayTime = 2000) {
+      this.textElement = textElement;
+      this.words = words;
+      this.text = "";
+      this.wordIndex = 0;
+      this.delayTime = parseInt(delayTime, 10);
+      this.type();
+      this.isDeleting = false;
+    }
+
+    type() {
+      const currentIndex = this.wordIndex % this.words.length;
+      const fullWord = this.words[currentIndex];
+      if (this.isDeleting) {
+        this.text = fullWord.substring(0, this.text.length - 1);
+      } else {
+        this.text = fullWord.substring(0, this.text.length + 1);
+      }
+
+      this.textElement.innerHTML = `<span class="type">${this.text}</span>`;
+
+      let typeSpeed = 100;
+
+      if (this.isDeleting) {
+        typeSpeed /= 2;
+      }
+
+      if (!this.isDeleting && this.text == fullWord) {
+        typeSpeed = this.delayTime;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.text == "") {
+        this.isDeleting = false;
+        this.wordIndex++;
+        typeSpeed = 500;
+      }
+
+      setTimeout(() => this.type(), typeSpeed);
+    }
+  }
 
   const textElement = document.querySelector(".typewriter ");
   const words = JSON.parse(textElement.getAttribute("type-words"));
